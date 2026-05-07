7c1663de7aa9427b785a7c7e5412cd794d7bf475
RomanGitMaster <roman.yasinovskyy29@gmail.com>
2026-05-07 13:38:17 -0400

    basic system is ready

---
+++ src/main/java/com/roma/automation/Planets.java
@@ -0,0 +1,21 @@
+package com.roma.automation;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Planets {
+    public static void main(String[] args) {
+        String star = "Sun";
+        System.out.println("Welcome to the Solar System with the star called " + star + ". Here you can see our planets: ");
+
+
+        List<String> planets = new ArrayList<>(List.of("Mercury", "Venus", "Earth", "Jupitor", "Saturn", "Uranus", "Neptune"));
+        for (var planet : planets) {
+            System.out.print(planet + " ");
+        }
+
+
+    }
+}
